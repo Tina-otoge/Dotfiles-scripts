a5b1ffad59ed12ecab840d80b749ca7aa2cdffef
Tina <skielred@gmail.com>
2023-10-09 01:28:35 +0200

    Add comments about sig check disable

--- steamdeck/setup/00_disable_readonly_and_populate_repos.sh
+++ steamdeck/setup/00_disable_readonly_and_populate_repos.sh
@@ -6,7 +6,11 @@ sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman-key --populate holo
 sudo pacman-key -u
-sudo pacman -Su
-sudo pacman -S --noconfirm --overwrite \* archlinux-keyring
-sudo /usr/bin/archlinux-keyring-wkd-sync || echo Done
+
+# Attempt at fixing broken signature on a package
+# sudo pacman -Su
+# sudo pacman -S --noconfirm --overwrite \* archlinux-keyring
+# sudo /usr/bin/archlinux-keyring-wkd-sync || echo Done
+
+# I finally gave up and just disabled sig checking
 sudo sed -i 's/SigLevel    = .*/SigLevel    = Never/g' /etc/pacman.conf
